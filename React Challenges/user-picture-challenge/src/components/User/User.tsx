5ce875411c83afc300232e9ba03f0785ad14561b
Eloy <eloymazza.ra@gmail.com>
2023-06-05 21:37:59 -0300

    user-picture-challenge: Finished user picture challenge

--- React Challenges/user-picture-challenge/src/components/User/User.tsx
+++ React Challenges/user-picture-challenge/src/components/User/User.tsx
@@ -8,8 +8,8 @@ const User: React.FC<Props> = ({ user }) => {
   if (!user) return <div>No users to display</div>;
 
   return (
-    <div>
-      <div>Name: {user.name}</div>
+    <div style={{ margin: 5 }}>
+      <div style={{ margin: 5 }}>{user.name}</div>
       <img src={user.pictureURL} alt='User Picture' />
     </div>
   );
--- React Challenges/user-picture-challenge/src/components/UserList/UserList.tsx
+++ React Challenges/user-picture-challenge/src/components/UserList/UserList.tsx
@@ -4,19 +4,37 @@ import useUsers from '../../hooks/useUsers';
 
 const UserList = () => {
   const { users, fetchUser, isLoading } = useUsers();
+  const [currentUserIndex, setCurrentUserIndex] = React.useState(0);
+
+  const currentUser = users[currentUserIndex];
+
+  const handleNextUser = async () => {
+    if (currentUserIndex === users.length - 1) {
+      await fetchUser();
+    }
+    setCurrentUserIndex((prev) => prev + 1);
+  };
+
+  const handlePrevUser = () => {
+    setCurrentUserIndex((prev) => prev - 1);
+  };
 
   return (
     <div>
-      <h2>User List</h2>
-      {isLoading && <div>Loading...</div>}
       {users.length === 0 ? (
         <button onClick={fetchUser}>Fetch User</button>
       ) : (
         <>
-          <h3>Current User:</h3>
-          <User user={users[0]} />
-          <button>Prev User</button>
-          <button>Next User</button>
+          <h3>Current User</h3>
+          {isLoading ? (
+            <div style={{ margin: 5 }}>Loading...</div>
+          ) : (
+            <User user={currentUser} />
+          )}
+          <button disabled={currentUserIndex === 0} onClick={handlePrevUser}>
+            Prev User
+          </button>
+          <button onClick={handleNextUser}>Next User</button>
         </>
       )}
     </div>
